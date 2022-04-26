18c2fe2c1b15d63dac71188b853f46217ce2a237
bazil <bazilkoolath7777@gmail.com>
2022-04-26 00:39:01 -0700

    registration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
+import { StudentRegistrationComponent } from './student-registration/student-registration.component';
 
 const routes: Routes = [
   {
     path:"",component:LoginComponent
   },{
     path:"dashboard",component:DashboardComponent
+  },{
+
+    path:"student-registration",component:StudentRegistrationComponent
+
   }
 ];
 
