182754e32d0df1686e470a7031ec7522b9c45c28
sahalsaman <sahal.saman@stunntech.com>
2023-09-24 00:13:55 +0530

    admin dashboard ui

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -4,9 +4,10 @@ import { AdminComponent } from './admin.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { StudentsAttendanceComponent } from './students-attendance/students-attendance.component';
 import { TimeTableComponent } from './time-table/time-table.component';
-import { AccountManagmentComponent } from './account-managment/account-managment.component';
-import { PaymentManagmentComponent } from './payment-managment/payment-managment.component';
-import { StudentsListViewComponent } from './account-managment/students-list-view/students-list-view.component';
+import { StudentsListViewComponent } from './students-list-view/students-list-view.component';
+import { TeachersListComponent } from './teachers-list/teachers-list.component';
+import { StudentsFeeComponent } from './students-fee/students-fee.component';
+import { TeachersPaymentComponent } from './teachers-payment/teachers-payment.component';
 
 
 const routes: Routes = [
@@ -21,15 +22,21 @@ const routes: Routes = [
   {
     path:"dashboard",
     component:DashboardComponent
+  }, {
+    path:"students-list",
+    component:StudentsListViewComponent
+  }, {
+    path:"teachers-list",
+    component:TeachersListComponent
+  }, {
+    path:"students-payment",
+    component:StudentsFeeComponent
+  }, {
+    path:"teachers-payment",
+    component:TeachersPaymentComponent
   },{
     path:'student-attendance',
     component:StudentsAttendanceComponent
-  },{
-    path:'account-managment',
-    component:AccountManagmentComponent
-  },{
-    path:'payment-managment',
-    component:PaymentManagmentComponent
   },{
     path:'time-table',
     component:TimeTableComponent
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -6,12 +6,11 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { StudentsAttendanceComponent } from './students-attendance/students-attendance.component';
-import { StudentsFeeComponent } from './payment-managment/students-fee/students-fee.component';
-import { TeachersListComponent } from './account-managment/teachers-list/teachers-list.component';
 import { TimeTableComponent } from './time-table/time-table.component';
-import { AccountManagmentComponent } from './account-managment/account-managment.component';
-import { PaymentManagmentComponent } from './payment-managment/payment-managment.component';
-
+import { StudentsFeeComponent } from './students-fee/students-fee.component';
+import { StudentsListViewComponent } from './students-list-view/students-list-view.component';
+import { TeachersListComponent } from './teachers-list/teachers-list.component';
+import { TeachersPaymentComponent } from './teachers-payment/teachers-payment.component';
 
 
 @NgModule({
@@ -20,12 +19,12 @@ import { PaymentManagmentComponent } from './payment-managment/payment-managment
     NavbarComponent,
     SidebarComponent,
     DashboardComponent,
+    StudentsListViewComponent,
+    TeachersListComponent,
     StudentsAttendanceComponent,
     StudentsFeeComponent,
-    AccountManagmentComponent,
-    TeachersListComponent,
-    PaymentManagmentComponent,
-    TimeTableComponent
+    TeachersPaymentComponent,
+    TimeTableComponent,
   ],
   imports: [
     CommonModule,
